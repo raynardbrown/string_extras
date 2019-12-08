b1cf28e64231965b0c16762b7f466d241bebae75
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-12-08 15:19:34 -0600

    feat: Add CharacterEncoder header

---
+++ inc/CharacterEncoder.h
@@ -0,0 +1,25 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: CharacterEncoder.h
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2019 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef CHARACTERENCODER_H_
+#define CHARACTERENCODER_H_
+
+class CharacterEncoder
+{
+  protected:
+    CharacterEncoder();
+
+  public:
+    ~CharacterEncoder();
+};
+
+#endif /* INC_CHARACTERENCODER_H_ */
