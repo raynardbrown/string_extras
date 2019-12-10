edcfd022f80c282b8cc9d61688e87f9cbd9804a9
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-12-09 23:09:45 -0600

    feat: Add the utf8 <==> utf16 interfaces

--- inc/CharacterEncoder.h
+++ inc/CharacterEncoder.h
@@ -13,6 +13,8 @@
 #ifndef CHARACTERENCODER_H_
 #define CHARACTERENCODER_H_
 
+#include <string>
+
 class CharacterEncoder
 {
   protected:
@@ -20,6 +22,33 @@ class CharacterEncoder
 
   public:
     ~CharacterEncoder();
+
+    /**
+     * Convert the specified string from UTF-8 to UTF-16. If the specified
+     * string is greater than the range of "int" in bytes, then the string is
+     * truncated.
+     *
+     * @param[in] utf8String a string encoded in UTF-8.
+     *
+     * @return the specified string encoded in UTF-16.
+     *
+     * @throws std::exception if specified string cannot be processed.
+     */
+    static std::wstring utf8ToUtf16(const std::string& utf8String);
+
+    /**
+     * Convert the specified string from UTF-16 to UTF-8. If the specified
+     * string is greater than the range of "int" in bytes, then the string is
+     * truncated.
+     *
+     * @param[in] utf16String a string encoded in UTF-16.
+     *
+     * @return the specified string encoded in UTF-8.
+     *
+     * @throws std::exception if specified string cannot be processed.
+     */
+    static std::string utf16ToUtf8(const std::wstring& utf16String);
+
 };
 
 #endif /* INC_CHARACTERENCODER_H_ */
