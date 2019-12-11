0fb7dae9ee4867e322453755f86707626ff1b5f8
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-12-10 23:09:20 -0600

    feat: Add CharacterEncoder source

---
+++ src/CharacterEncoder.cpp
@@ -0,0 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: CharacterEncoder.cpp
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2019 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "CharacterEncoder.h"
+
+CharacterEncoder::CharacterEncoder()
+{
+}
+
+CharacterEncoder::~CharacterEncoder()
+{
+}
